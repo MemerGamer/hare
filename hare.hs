71a3851d0b64e9931eca2a5b3690d49419bf6832
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-03-31 11:43:55 +0300

    working http server for html css and js working on image loading

--- hare.hs
+++ hare.hs
@@ -1,7 +1,10 @@
 {-# OPTIONS_GHC -Wno-deprecations #-}
 
 import Control.Monad
+import qualified Data.ByteString as BS
 import Data.ByteString.Char8 (hPutStrLn, pack, unpack)
+import qualified Data.ByteString.Char8 as BSC
+import Data.List (intercalate)
 import GHC.IO.Handle
   ( BufferMode (NoBuffering),
     hClose,
@@ -25,6 +28,8 @@ import Network.Socket
     socketToHandle,
     withSocketsDo,
   )
+import System.Directory (doesFileExist)
+import System.FilePath (takeExtension, (</>))
 
 main = do
   let port = 8080
@@ -47,11 +52,62 @@ server port hostname backlog = withSocketsDo $ do
     hSetBuffering handle NoBuffering
     request <- hGetLine handle
     putStrLn $ "Received request: " ++ request
-    hPutStrLn handle (pack "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello")
-    hClose handle
+    let filePath = getFilePath request
+    fileExists <- doesFileExist filePath
+    if fileExists
+      then do
+        contents <- BS.readFile filePath
+        let contentType = getContentType filePath
+        let response =
+              intercalate
+                "\r\n"
+                [ "HTTP/1.1 200 OK",
+                  "Content-Type: " ++ contentType,
+                  "Content-Length: " ++ show (BS.length contents),
+                  "",
+                  ""
+                ]
+        if isImage contentType
+          then do
+            BSC.hPutStrLn handle (BSC.pack response)
+            BS.hPutStr handle contents
+          else do
+            BSC.hPutStrLn handle (BSC.pack response)
+            BSC.hPutStrLn handle (BSC.pack $ unpack contents)
+      else do
+        hPutStrLn handle (pack "HTTP/1.1 404 Not Found\r\n\r\n")
+        hClose handle
+
+isImage :: String -> Bool
+isImage contentType =
+  contentType == "image/jpeg"
+    || contentType == "image/png"
+    || contentType == "image/gif"
 
 resolve :: String -> PortNumber -> IO AddrInfo
 resolve hostname port = do
   let hints = defaultHints {addrSocketType = Stream}
   addr : _ <- getAddrInfo (Just hints) (Just hostname) (Just $ show port)
   return addr
+
+getFilePath :: String -> FilePath
+getFilePath request =
+  let parts = words request
+      path = parts !! 1
+      normalizedPath =
+        if head path == '/'
+          then tail path
+          else path
+   in "sites/" </> normalizedPath
+
+getContentType :: FilePath -> String
+getContentType path =
+  let ext = takeExtension path
+   in case ext of
+        ".html" -> "text/html"
+        ".css" -> "text/css"
+        ".js" -> "text/javascript"
+        ".png" -> "image/png"
+        ".jpg" -> "image/jpeg"
+        ".jpeg" -> "image/jpeg"
+        _ -> "application/octet-stream"
