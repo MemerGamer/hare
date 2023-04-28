c9a5f662715963ee66706b91aafe26f4a6a3511c
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-04-28 10:36:47 +0300

    extended the javascript sample code

--- sites/js/main.js
+++ sites/js/main.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// This file is a demonstration that the webserver can serve js files.
+
+// Simple Hello World!
+console.log("Hello Hare!");
+
+// Basic arithmetic
+let a = 5, b = 10;
+
+console.log("a: " + a);
+console.log("b: " + b);
+
+let sum = a+b, diff = a-b, prod = a*b, div = a/b;
+
+console.log("a+b = " + sum);
+console.log("a-b = " + diff);
+console.log("a*b = " + prod);
+console.log("a/b = " + div);
